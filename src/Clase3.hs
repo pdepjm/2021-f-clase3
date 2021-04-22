fc6fed200a44872f1c604954044814d1852e53c3
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2021-04-22 12:42:01 -0300

    Agrego lo hecho en clase

--- src/Clase3.hs
+++ src/Clase3.hs
@@ -1,3 +1,71 @@
 module Clase3 where
 import PdePreludat
 
+vocales = "aeiouAEIOU"
+
+esVocal letra = elem letra vocales
+
+esConsonante = not.esVocal
+
+f x y = x && not y
+
+vocales :: String
+esConsonante :: Char -> Bool
+
+quedateElPrimero :: a -> b -> a
+quedateElPrimero x y = x
+
+negameElPrimero x y = not x
+
+identidad :: a -> a
+identidad x = x
+
+esMinuscula = between 'a' 'z'
+
+esPar = even
+
+
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+between :: Ord a => a -> a -> a -> Bool
+between bajo alto medio = bajo <= medio && medio <= alto
+
+
+
+
+
+-- enEdadEscolar cuando está entre 3 y 17 años
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -2,9 +2,17 @@ module Spec where
 import PdePreludat
 import Clase3
 import Test.Hspec
-
+{-
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "saraza" $ do
-     it "asdf" $ do
-        expectationFailure "Falta implementar" 
+  describe "Test de esBisiesto" $ do
+     it "Un año multiplo de 400 debe ser bisiesto" $ do
+        esBisiesto 2000 `shouldBe` True
+     it "Un año múltiplo de 4 debe ser bisiesto" $ do
+        esBisiesto 2004 `shouldBe` True
+        -- esBisiesto 2016 `shouldBe` True
+     it "Un año múltiplo de 100 (que no es de 400) no debe ser bisiesto " $ do
+        esBisiesto 1900 `shouldBe` False
+     it "Un año en ninguno de los casos anteriores no debe ser bisiesto " $ do
+        esBisiesto 2021 `shouldBe` False
+-}
